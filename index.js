8434cf55e9c4b74e6fc4578f52b39b89d19b1ec7
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-11-06 22:45:15 +0600

    Retreive features foods data from database

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-console.log(process.env.DB_PASS);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9ylutr.mongodb.net/?retryWrites=true&w=majority"`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,18 +18,32 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   }
-});
+}); 
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+      // Send a ping to confirm a successful connection
+      
+
+      const database = client.db("foodCharity");
+      const availableFoods = database.collection("availableFoods");
+
+
+      app.get('/api/availableFoods', async(req, res) => {
+          const cursor = availableFoods.find();
+          const result = await cursor.toArray();
+          console.log(result); 
+          res.send(result);  
+      })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
