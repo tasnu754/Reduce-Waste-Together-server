b808d07d611b817f5793a2071401e285db01e3b2
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-11-08 01:38:29 +0600

    Delete a food item from database using id params and query

--- index.js
+++ index.js
@@ -75,6 +75,14 @@ async function run() {
  
     })
 
+    app.delete('/api/availableFoods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await availableFoods.deleteOne(query); 
+      res.send(result);
+
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
